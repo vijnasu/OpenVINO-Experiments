773bbadba9d695ba6081884dde9cd041e3b11d09
Hemanth Kumar G <hemanthjois@gmail.com>
2019-02-27 19:11:57 +0530

    Add files via upload

---
+++ Yolov3/Yolov3_files_downloader.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+import os
+
+def progress(blockNum, blockSize, totalSize):
+    read = blockNum * blockSize
+    if totalSize > 0:
+        percent = read * 100 / totalSize
+        s = "\r%5.0f%% " % (percent)
+        sys.stderr.write(s)
+        if read >= totalSize:
+            sys.stderr.write("\n")
+    else:
+        sys.stderr.write("read %d\n" % (read,))
+
+urls = ['https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names',
+        'https://pjreddie.com/media/files/yolov3.weights']
+
+for url in urls:
+    filename = url[url.rfind("/") + 1: ]
+    print ('Downloading ' + filename)
+    if sys.version_info.major > 2:
+        import urllib.request
+        urllib.request.urlretrieve(url, filename, progress)
+    else:
+        import urllib
+        urllib.urlretrieve(url, filename, progress)
